b33ac2fb12b0767682ed2012c86bbbc3ceccf3b3
Jakub Jukl <jukljakub@gmail.com>
2023-12-23 17:18:58 +0100

    comments

--- MinesweeperAI.py
+++ MinesweeperAI.py
@@ -4,31 +4,38 @@ from collections import deque
 class MinesweeperAI():
 
     # if AI shouldn't know about number of mines, pass 0
-    def __init__(self, height=8, width=8, mines=8):
+    def __init__(self, height: int, width: int, mines: int = 0):
+        # Set initial width and height of the board and optional number of mines
         self.height = height
         self.width = width
         self.num_of_mines = mines
 
+        # to keep track of moves made and flags placed
         self.moves_made = set()
         self.flags_placed = set()
 
+        # discovered mines and safes
         self.mines = set()
         self.safes = set()
 
+        # knowledge base -> set of sentences
         self.knowledge : set[Sentence] = set()
 
         # if True, calculation of safe moves happens as soon as possible
         self.search_asap = True
 
+        # initialize board, so we can access random moves faster
         self.board = set()
         for i in range(self.height):
             for j in range(self.width):
                 self.board.add((i, j))
         
+        # if we know number of mines, add sentence with all cells and number of mines
         if self.num_of_mines > 0:
             self.knowledge.add(Sentence(self.board.copy(), self.num_of_mines))
 
-    def __get_neighbours(self, cell: tuple[int, int]):
+    def __get_neighbours(self, cell: tuple[int, int]) -> set[tuple[int, int]]:
+        """Returns a set of all neighbours of a cell"""
         neighbours = set()
         y, x = cell
         for i in range(y - 1, y + 2):
@@ -38,6 +45,7 @@ class MinesweeperAI():
         return neighbours
 
     def add_knowledge(self, cell: tuple[int, int], count: int):
+        """Notify AI that cell is a neighbour of (count) mines"""
         self.moves_made.add(cell)
         self.safes.add(cell)
         neighbours = self.__get_neighbours(cell)
@@ -59,6 +67,8 @@ class MinesweeperAI():
                 subset_any_last_run = self.__create_subsets()
 
     def __remove_known(self) -> bool:
+        # store sentences, that were not traversed with current known set of mines and safes in a deque
+        # so they can be traversed in an order
         to_traverse = deque(self.knowledge)
         traversed = set()
         resolved_any = False
@@ -68,9 +78,13 @@ class MinesweeperAI():
             sentence.mark_safes(self.safes)
             sentence.mark_mines(self.mines)
             if sentence.is_resolved():
+                # if sentence is resolved, we do not wish to add it to the knowledge base
+                # but we do wish to update the knowledge base with the new safes and mines
                 resolved_any = True
                 self.safes.update(sentence.known_safes())
                 self.mines.update(sentence.known_mines())
+                # since we updated known mines and safes, we need to recheck all previously traversed sentences
+                # so we add them to the deque and reset the traversed set
                 to_traverse.extendleft(traversed)
                 traversed = set()
             else:
